3028ab6a8614500831bf23bcc240fcd392ccb8c4
mkruneva <marta.kruneva@gmail.com>
2018-01-05 11:01:51 +0000

    test component created manually

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-//import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '@angular/material'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
@@ -9,11 +8,13 @@ import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
