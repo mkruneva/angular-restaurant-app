25c29321c2c00baeab23f4bb8b87a4afadb465ee
mkruneva <marta.kruneva@gmail.com>
2018-01-08 11:32:12 +0000

    using dishService to fetch dishes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './services/dish.service';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +25,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MaterialModule,
     FlexLayoutModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ DishService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
