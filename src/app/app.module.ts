96db0dd89c3abaf3cc6e4c31df9f459838666d7e
mkruneva <marta.kruneva@gmail.com>
2018-01-08 11:55:49 +0000

    header and footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,20 @@ import 'hammerjs';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 import { DishService } from './services/dish.service';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
