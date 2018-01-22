462fc54bc06f54e855b03f35d4fed2dbedf27718
mkruneva <marta.kruneva@gmail.com>
2018-01-22 10:01:14 +0000

    promotions fetched from server

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -1,27 +1,35 @@
 import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion';
-import { PROMOTIONS } from '../shared/promotions';
+import { baseURL} from '../shared/baseurl';
+import { Http, Response } from '@angular/http';
+import { ProcessHttpmsgService } from './process-httpmsg.service';
+import { PROMOTIONS } from '../shared/promotions'; // remove
 import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class PromotionService {
 
-  constructor() { }
+  constructor(private http: Http, private procssHTTPservice: ProcessHttpmsgService) { }
 
   getPromotions(): Observable<Promotion[]> {
-    return Observable.of(PROMOTIONS).delay(2000);
+    return this.http.get(baseURL + 'promotions')
+             .map(res => this.procssHTTPservice.extractData(res));
   }
 
   getPromotion(id: number): Observable<Promotion> {
-    return Observable.of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).delay(2000);
+    return  this.http.get(baseURL + 'promotions/' + id)
+                    .map(res => this.procssHTTPservice.extractData(res));
   }
 
   getFeaturedPromotion(): Observable<Promotion> {
-    return Observable.of(PROMOTIONS.filter((promo) => promo.featured)[0]).delay(2000);
+    return this.http.get(baseURL + 'promotions?featured=true')
+                    .map(res => this.procssHTTPservice.extractData(res)[0]);
   }
 
 }
