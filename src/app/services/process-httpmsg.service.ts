f30138fe2541a141f7feaeb3bccf4e77f62c5511
mkruneva <marta.kruneva@gmail.com>
2018-01-22 10:18:31 +0000

    hangleError processHTTPrequest method

--- src/app/services/process-httpmsg.service.ts
+++ src/app/services/process-httpmsg.service.ts
@@ -3,6 +3,8 @@ import { Http, Response } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 
+import 'rxjs/add/observable/throw';
+
 @Injectable()
 export class ProcessHttpmsgService {
 
@@ -12,6 +14,20 @@ export class ProcessHttpmsgService {
     let body = res.json();
 
     return body || {};
+  }
+
+  public handleError(error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+
+    return Observable.throw(errMsg);
+
   }
 
 }
