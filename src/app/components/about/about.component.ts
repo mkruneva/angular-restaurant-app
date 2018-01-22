03740e2d809f7cae89a4357628bcc48f8c49e5cb
mkruneva <marta.kruneva@gmail.com>
2018-01-22 09:55:48 +0000

    leaders fetched from server

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 
 import { Leader } from '../../shared/leader';
 import { LeaderService } from '../../services/leader.service';
@@ -10,9 +10,9 @@ import { LeaderService } from '../../services/leader.service';
 })
 export class AboutComponent implements OnInit {
 
-  leaders: Leader[]; 
+  leaders: Leader[];
 
-  constructor( private leaderService: LeaderService ) { }
+  constructor( private leaderService: LeaderService, @Inject('BaseURL') private BaseURL ) { }
 
   ngOnInit() {
     this.leaderService.getLeaders()
--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,23 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
-import { LEADERS } from '../shared/leaders';
+import { baseURL} from '../shared/baseurl';
+import { Http, Response } from '@angular/http';
+import { ProcessHttpmsgService } from './process-httpmsg.service';
 import { Observable } from 'rxjs/Observable';
+
 
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class LeaderService {
 
-  constructor() { }
+  constructor(private http: Http, private procssHTTPservice: ProcessHttpmsgService) { }
 
   getLeaders(): Observable<Leader[]> {
-      return Observable.of(LEADERS).delay(2000);
+      return this.http.get(baseURL + 'leaders')
+             .map(res => this.procssHTTPservice.extractData(res));
   }
 
   getFeaturedLeader(): Observable<Leader> {
-      return  Observable.of(LEADERS.filter((leader) => leader.featured)[0]).delay(2000);
+      return this.http.get(baseURL + 'leaders?featured=true')
+                    .map(res => this.procssHTTPservice.extractData(res)[0]);
   }
 
 }
