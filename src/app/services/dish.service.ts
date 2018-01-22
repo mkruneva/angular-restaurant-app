0e5694d7c8026b30ed5a1507a056d3239b742ccb
mkruneva <marta.kruneva@gmail.com>
2018-01-22 12:42:27 +0000

    dishservice cleanup

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,23 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
-import { baseURL } from '../shared/baseurl';
-import { Http, Response } from '@angular/http';
-import { ProcessHttpmsgService } from './process-httpmsg.service';
 import { Observable } from 'rxjs/Observable';
-
 import { RestangularModule, Restangular } from 'ngx-restangular';
-
-import 'rxjs/add/operator/delay';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
-
 
 @Injectable()
 export class DishService {
 
-  constructor(private restangular: Restangular, private procssHTTPservice: ProcessHttpmsgService) { }
+  constructor(private restangular: Restangular) { }
 
-  // using rxjs
   getDishes() {
     return this.restangular.all('dishes').getList();
   }
