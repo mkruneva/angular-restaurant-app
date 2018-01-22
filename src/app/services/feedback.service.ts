32dd7438d7394c0cc56dbf1942f115525e1836bd
mkruneva <marta.kruneva@gmail.com>
2018-01-22 13:27:55 +0000

    feedback service

---
+++ src/app/services/feedback.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Feedback } from '../shared/feedback';
+import { Observable } from 'rxjs/Observable';
+import { RestangularModule, Restangular } from 'ngx-restangular';
+
+@Injectable()
+export class FeedbackService {
+
+  constructor(private restangular: Restangular) { }
+
+  getFeedback() {
+    return this.restangular.all('feedback').getList();
+  }
+
+}
