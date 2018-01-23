14f3638d940f3e123e2f689ca859e39460f7182f
mkruneva <marta.kruneva@gmail.com>
2018-01-23 14:14:09 +0000

    cleanup; next step -> implement better routing animation

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -7,10 +7,10 @@ import { AboutComponent } from '../components/about/about.component';
 import { ContactComponent } from '../components/contact/contact.component';
 
 export const routes: Routes = [
-	{ path: 'home', component: HomeComponent },
-	{ path: 'about', component: AboutComponent },
-	{ path: 'contact', component: ContactComponent },
-	{ path: 'menu', component: MenuComponent },
-	{ path: 'dishdetail/:id', component: DishdetailComponent },
-	{ path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'dishdetail/:id', component: DishdetailComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
