8e7ae06238eb0a953e196901b20449009d2d23d6
mkruneva <marta.kruneva@gmail.com>
2018-01-24 13:43:36 +0000

    menu test dom el check

--- src/app/components/menu/menu.component.spec.ts
+++ src/app/components/menu/menu.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 import { BrowserAnimationBuilder } from '@angular/platform-browser/animations/src/animation_builder';
 import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -56,5 +58,17 @@ describe('MenuComponent', () => {
     expect(component.dishes[0].name).toBe('Uthappizza');
     expect(component.dishes[3].comments).toBeTruthy();
   });
+
+  it('should use dishes in the template', () => {
+    fixture.detectChanges();
+
+    let de:      DebugElement;
+    let el:      HTMLElement;
+    de = fixture.debugElement.query(By.css('h1'));
+    el = de.nativeElement;
+
+    expect(el.textContent).toContain(DISHES[0].name.toUpperCase());
+
+    });
 
 });
