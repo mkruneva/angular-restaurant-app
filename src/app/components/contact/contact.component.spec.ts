d9297f6d2de37cf57fb1e6e19724733d0f282575
mkruneva <marta.kruneva@gmail.com>
2018-01-24 20:47:07 +0000

    contact test

--- src/app/components/contact/contact.component.spec.ts
+++ src/app/components/contact/contact.component.spec.ts
@@ -1,6 +1,26 @@
+import { ViewChild } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationBuilder } from '@angular/platform-browser/animations/src/animation_builder';
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormGroupDirective } from '@angular/forms';
+
+import { Feedback, ContactType } from '../../shared/feedback';
+import { FeedbackService } from '../../services/feedback.service';
 
 import { ContactComponent } from './contact.component';
+
+class MdDialogRefMock {
+}
+
+const FeedbackServiceStub = {
+  submitFeedback: function() {
+    // does something
+  }
+};
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
@@ -8,9 +28,15 @@ describe('ContactComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContactComponent ]
+      declarations: [ ContactComponent ],
+      imports: [ BrowserAnimationsModule, MaterialModule, FlexLayoutModule,
+        FormsModule, ReactiveFormsModule ],
+      providers: [ {provide: FeedbackService, useValue: FeedbackServiceStub }, FormBuilder ]
     })
     .compileComponents();
+
+    let feedbackService = TestBed.get(FeedbackService);
+
   }));
 
   beforeEach(() => {
